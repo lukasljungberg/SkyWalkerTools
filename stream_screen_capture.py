a7c87114ad2853fa21ab34c2ee4fa2b411a04223
Lukas Ljungberg <lukas-ljungberg@outlook.com>
2023-12-10 16:43:19 +0100

    Update stream_screen_capture.py

--- stream_screen_capture.py
+++ stream_screen_capture.py
@@ -2,31 +2,45 @@ import sys
 import pyautogui
 import imageio
 import time
-import validators
+import subprocess
 
 
 def capture_and_stream(rtmp_server_url):
     try:
-        # Create a writer to the RTMP server
-        writer = imageio.get_writer(rtmp_server_url, fps=1)
+        # Use subprocess to run ffmpeg command for streaming
+        cmd = [
+            "ffmpeg",
+            "-f", "image2pipe",
+            "-r", "1",  # Adjust the frame rate as needed
+            "-s", "1920x1080",  # Adjust the screen resolution as needed
+            "-i", "-",  # Input from pipe
+            "-c:v", "libx264",
+            "-b:v", "512k",  # Adjust the bitrate as needed
+            "-f", "flv",
+            rtmp_server_url
+        ]
+
+        ffmpeg_process = subprocess.Popen(cmd, stdin=subprocess.PIPE)
 
         while True:
             # Capture the screen
             screenshot = pyautogui.screenshot()
 
-            # Convert the screenshot to a numpy array
-            img_array = imageio.core.util.Array(screenshot)
+            # Convert the screenshot to bytes
+            img_byte_array = screenshot.tobytes()
 
-            # Add the frame to the writer
-            writer.append_data(img_array)
+            # Write the bytes to the ffmpeg process stdin
+            ffmpeg_process.stdin.write(img_byte_array)
 
             # Adjust the delay according to your needs
             time.sleep(1)
+
     except KeyboardInterrupt:
         print("Screen capture and streaming stopped.")
     finally:
-        writer.close()
-
+        if ffmpeg_process:
+            ffmpeg_process.stdin.close()
+            ffmpeg_process.wait()
 
 if __name__ == "__main__":
     stream_url = sys.argv[1]
