62e2b47a4f222b7fc5a85307f9f2e4a723571895
Lukas Ljungberg <lukas-ljungberg@outlook.com>
2023-12-13 18:43:26 +0100

    Update ssl_decryption.py
    trying fix

--- ssl_decryption.py
+++ ssl_decryption.py
@@ -1,39 +1,45 @@
-from cryptography.hazmat.primitives.serialization import load_pem_private_key
-from cryptography.hazmat.primitives.asymmetric.padding import AsymmetricPadding
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import serialization, hashes
+from cryptography.hazmat.primitives.asymmetric import padding
 from scapy.all import *
-from scapy.layers.tls.all import *
 
 
 def get_private_key(file_path):
     key = None
     with open(file_path, 'rb') as key_file:
-        key = key_file.read()
+        key_data = key_file.read()
 
-    return load_pem_private_key(key, None)
+    return serialization.load_pem_private_key(
+        key_data,
+        password=None,
+        backend=default_backend()
+    )
 
 
 def decrypt(pcap_path, key_path):
     load_layer('tls')
     packets = rdpcap(pcap_path)
-    key = get_private_key(key_path)
+    private_key = get_private_key(key_path)
 
     for packet in packets:
-        tls_record = None
-        print(packet)
-        try:
+        if Raw in packet:
             tls_record = packet[Raw]
-            print(tls_record)
-        except Exception as exc:
-            print(exc)
-        if hasattr(tls_record, 'load'):
             try:
-                decrypted_data = key.decrypt(tls_record.load)
+                decrypted_data = private_key.decrypt(
+                    tls_record.load,
+                    padding.OAEP(
+                        mgf=padding.MGF1(algorithm=hashes.SHA256()),
+                        algorithm=hashes.SHA256(),
+                        label=None
+                    )
+                )
                 print("Decrypted Data:", decrypted_data.decode('utf-8', 'ignore'))
             except Exception as exc:
                 print("Exc: ", exc)
 
 
 if __name__ == '__main__':
+    import sys
     pcap = sys.argv[1]
     key = sys.argv[2]
     try:
