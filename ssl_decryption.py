83c8d62cd5a75bdd2ab12e62b4a12cbb2f259611
Lukas Ljungberg <lukas-ljungberg@outlook.com>
2023-12-13 17:55:04 +0100

    Update ssl_decryption.py

--- ssl_decryption.py
+++ ssl_decryption.py
@@ -1,6 +1,7 @@
 from cryptography.hazmat.primitives.serialization import load_pem_private_key
 from cryptography.hazmat.primitives.asymmetric.padding import AsymmetricPadding
 from scapy.all import *
+from scapy.layers.tls.all import *
 
 
 def get_private_key(file_path):
@@ -12,13 +13,14 @@ def get_private_key(file_path):
 
 
 def decrypt(pcap_path, key_path):
+    load_layer('tls')
     packets = rdpcap(pcap_path)
     key = get_private_key(key_path)
 
     for packet in packets:
         tls_record = None
         try:
-            tls_record = packet[Raw]
+            tls_record = packet[TLS]
             print(tls_record)
         except Exception as exc:
             print(exc)
